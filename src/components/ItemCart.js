de09c948b24c3898dabf6a1087667f9a361c6303
Fernando <pupygreen@gmail.com>
2022-10-11 15:08:19 -0300

    Estilização de componentes

--- src/components/ItemCart.js
+++ src/components/ItemCart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import '../styles/ItemCart.css';
 
 class ItemCart extends React.Component {
   render() {
@@ -7,12 +8,31 @@ class ItemCart extends React.Component {
     const { title, price, thumbnail } = cartItensArray;
     console.log(cartItensArray);
     return (
-      <div className="intemsCart">
-        {' '}
-        <p data-testid="shopping-cart-product-name">{title}</p>
-        <p>{price}</p>
-        <img src={ thumbnail } alt={ title } />
-        <p data-testid="shopping-cart-product-quantity">1</p>
+      <div className="divItemscart">
+        <div className="intemsCart">
+          <div className="image">
+            <button data-testid="remove-product" type="button">Excluir</button>
+            <img src={ thumbnail } alt={ title } />
+          </div>
+          <div className="description">
+            <p
+              className="itemsCart__title"
+              data-testid="shopping-cart-product-name"
+            >
+              {title}
+            </p>
+          </div>
+          <div className="quantity">
+            <button type="button" data-testid="product-increase-quantity">-</button>
+            <p data-testid="shopping-cart-product-quantity">1</p>
+            <button type="button" data-testid="product-decrease-quantity">+</button>
+          </div>
+          <p>
+            R$
+            {' '}
+            {price}
+          </p>
+        </div>
       </div>
     );
   }
